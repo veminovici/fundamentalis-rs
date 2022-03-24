46c5856af8c8b46aa76bd41bdc12c3821f01795d
veminovici <veminovici@hotmail.com>
2022-03-24 14:33:36 +0200

    add logging example

---
+++ utils/logging/src/main.rs
@@ -0,0 +1,10 @@
+// cargo run --bin logging
+
+fn execute_query(query: &str) {
+    log::debug!("Executing query:{}", query);
+}
+
+fn main() {
+    env_logger::init();
+    execute_query("INSERT");
+}
